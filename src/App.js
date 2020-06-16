84be42b9d08c71ec6f07fd1b74de29a83f0d29b7
Adam SOudani <asoudani@email.com>
2020-06-16 00:26:27 -0700

    card images and nav fixed

--- src/App.js
+++ src/App.js
@@ -25,70 +25,70 @@ function App() {
     <div class="row row-cols-1 row-cols-md-3">
       <div class="col mb-4">
         <div class="card">
-          <img src="..." class="card-img-top" alt="..."/>
+          <img src="https://www.liveabout.com/thmb/AZq8udcF7H9mN1-TSdwJfRoqxOc=/300x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/familyGuy__Stewie_para_72-56a00a5f3df78cafda9fc0cc.jpg" class="card-img-top" alt="..."/>
   </div>
         </div>
         <div class="col mb-4">
           <div class="card">
-            <img src="..." class="card-img-top" alt="..."/>
+            <img src="https://www.liveabout.com/thmb/YO0P_2oldbjBfdktrf0l2TRgV6A=/2550x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/8acx14_sc379_0542_hires2-57a96c2a5f9b58974ae107fd.jpg" class="card-img-top" alt="..."/>
   </div>
           </div>
           <div class="col mb-4">
             <div class="card">
-              <img src="..." class="card-img-top" alt="..."/>
+              <img src="https://www.liveabout.com/thmb/XNzQN4s0AiiEZvi5OrJcQYtx6lQ=/1950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy_pewterschmidt-56a010053df78cafda9fdf38.jpg" class="card-img-top" alt="..."/>
   </div>
             </div>
             <div class="col mb-4">
               <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
+                <img src="https://www.liveabout.com/thmb/yvYr2GGwOCeBdqj3wwFMJemQhLA=/2550x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cacx01_061_03b_0337_hires2-56a010075f9b58eba4aeb9c5.jpg" class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
               <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
+                <img src="https://www.liveabout.com/thmb/9hDbpua-5jQG7pH_c-16yF5H0F4=/300x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cleveland_v2Fb080309-56a00cf55f9b58eba4aead78.jpg" class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
               <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
+                <img src="https://www.liveabout.com/thmb/1k4LHSdZPiQNnbxMXT_xcXwRX24=/475x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuyQuagmireArt_v1fa-57a96bf33df78cf459bb222a.jpg" class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
               <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
+                <img src="https://www.liveabout.com/thmb/D6_8tRfI1EJ2UHXQYKY5UoNRUUI=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Chris_f-56a00aff3df78cafda9fc740.jpg" class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
               <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
+                <img src="https://www.liveabout.com/thmb/_q3WDsOk_KSBGoS8sh2LU2-FFhM=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Meg_f-56a00b005f9b58eba4aea214.jpg" class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
               <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
+                <img src="https://www.liveabout.com/thmb/BZpbhDgZrMxOUdqqWArsayPicz8=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Brian_f-56a00b003df78cafda9fc74f.jpg" class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
               <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
+                <img src="https://www.liveabout.com/thmb/H9V_z_oYhbKldDJJUL53tVN36cQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/peter_2008_v2F_hires1-56a00f083df78cafda9fdcb6.jpg" class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
               <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
+                <img src="https://www.liveabout.com/thmb/qL9NuJ5lmmRzMyFngIz9SyYjSiw=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Lois_f-56a00b003df78cafda9fc743.jpg" class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
               <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
+                <img src="https://www.liveabout.com/thmb/fc5QH6AXdNtQRwuaQevxSSiwp1k=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Stewie1_f-56a00b003df78cafda9fc746.jpg" class="card-img-top" alt="..."/>
   </div>
               </div>
 
