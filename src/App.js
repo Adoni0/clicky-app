0717527a05f3f5a32fd0f0cf59beabf4621495b7
Adam SOudani <asoudani@email.com>
2020-06-16 16:03:25 -0700

    card styling

--- src/App.js
+++ src/App.js
@@ -14,81 +14,80 @@ function App() {
       </nav>
 
       <div className="jumbotron .jumbotron-fluid .container-fluid ">
-        <h1 className="display-4">Hello, world!</h1>
-        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <h1 className="display-4">Clicky Game!</h1>
+        <p className="lead"><i>Family Guy Edition</i></p>
         <hr className="my-4"></hr>
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        <p>Click on an image to earn points, but don't click on the same image twice!</p>
       </div>
 
 
     <div class="row row-cols-1 row-cols-md-3">
       <div class="col mb-4">
-        <div class="card">
-          <img src="https://www.liveabout.com/thmb/AZq8udcF7H9mN1-TSdwJfRoqxOc=/300x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/familyGuy__Stewie_para_72-56a00a5f3df78cafda9fc0cc.jpg" class="card-img-top" alt="..."/>
+        <div class="card left" id='stewie1'>
+          <img src="https://www.liveabout.com/thmb/AZq8udcF7H9mN1-TSdwJfRoqxOc=/300x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/familyGuy__Stewie_para_72-56a00a5f3df78cafda9fc0cc.jpg" id='stew1' class="card-img-top" alt="..."/>
   </div>
         </div>
         <div class="col mb-4">
-          <div class="card">
-            <img src="https://www.liveabout.com/thmb/YO0P_2oldbjBfdktrf0l2TRgV6A=/2550x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/8acx14_sc379_0542_hires2-57a96c2a5f9b58974ae107fd.jpg" class="card-img-top" alt="..."/>
+          <div class="card center" id='herbert'>
+            <img src="https://sottai.files.wordpress.com/2010/04/herbert-family-guy.jpg" id='herb' class="card-img-top" alt="..."/>
   </div>
           </div>
           <div class="col mb-4">
-            <div class="card">
-              <img src="https://www.liveabout.com/thmb/XNzQN4s0AiiEZvi5OrJcQYtx6lQ=/1950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy_pewterschmidt-56a010053df78cafda9fdf38.jpg" class="card-img-top" alt="..."/>
+            <div class="card right" id='carter'>
+              <img src="https://i.pinimg.com/originals/b7/5e/bb/b75ebba8e88fc3c338a82bb1876643e9.jpg" class="card-img-top" alt="..."/>
   </div>
             </div>
             <div class="col mb-4">
-              <div class="card">
-                <img src="https://www.liveabout.com/thmb/yvYr2GGwOCeBdqj3wwFMJemQhLA=/2550x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cacx01_061_03b_0337_hires2-56a010075f9b58eba4aeb9c5.jpg" class="card-img-top" alt="..."/>
+              <div class="card left" id='joe'>
+                <img src="https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-family-guy-Patrick-Warburton.jpg" class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
-              <div class="card">
-                <img src="https://www.liveabout.com/thmb/9hDbpua-5jQG7pH_c-16yF5H0F4=/300x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cleveland_v2Fb080309-56a00cf55f9b58eba4aead78.jpg" class="card-img-top" alt="..."/>
+              <div class="card center" id='cleaveland'>
+                <img src="https://www.liveabout.com/thmb/9hDbpua-5jQG7pH_c-16yF5H0F4=/300x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cleveland_v2Fb080309-56a00cf55f9b58eba4aead78.jpg" id='cleave' class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
-              <div class="card">
-                <img src="https://www.liveabout.com/thmb/1k4LHSdZPiQNnbxMXT_xcXwRX24=/475x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuyQuagmireArt_v1fa-57a96bf33df78cf459bb222a.jpg" class="card-img-top" alt="..."/>
+              <div class="card right" id='quagmire'>
+                <img src="https://www.liveabout.com/thmb/1k4LHSdZPiQNnbxMXT_xcXwRX24=/475x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuyQuagmireArt_v1fa-57a96bf33df78cf459bb222a.jpg" id='quag' class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
-              <div class="card">
-                <img src="https://www.liveabout.com/thmb/D6_8tRfI1EJ2UHXQYKY5UoNRUUI=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Chris_f-56a00aff3df78cafda9fc740.jpg" class="card-img-top" alt="..."/>
+              <div class="card left" id='chris'>
+                <img src="https://www.liveabout.com/thmb/D6_8tRfI1EJ2UHXQYKY5UoNRUUI=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Chris_f-56a00aff3df78cafda9fc740.jpg" id='chrs' class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
-              <div class="card">
-                <img src="https://www.liveabout.com/thmb/_q3WDsOk_KSBGoS8sh2LU2-FFhM=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Meg_f-56a00b005f9b58eba4aea214.jpg" class="card-img-top" alt="..."/>
+              <div class="card center" id='meg'>
+                <img src="https://www.liveabout.com/thmb/_q3WDsOk_KSBGoS8sh2LU2-FFhM=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Meg_f-56a00b005f9b58eba4aea214.jpg" id='megatron' class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
-              <div class="card">
-                <img src="https://www.liveabout.com/thmb/BZpbhDgZrMxOUdqqWArsayPicz8=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Brian_f-56a00b003df78cafda9fc74f.jpg" class="card-img-top" alt="..."/>
+              <div class="card right" id='brian'>
+                <img src="https://www.liveabout.com/thmb/BZpbhDgZrMxOUdqqWArsayPicz8=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Brian_f-56a00b003df78cafda9fc74f.jpg" id='bri' class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
-              <div class="card">
-                <img src="https://www.liveabout.com/thmb/H9V_z_oYhbKldDJJUL53tVN36cQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/peter_2008_v2F_hires1-56a00f083df78cafda9fdcb6.jpg" class="card-img-top" alt="..."/>
+              <div class="card left" id='peter'>
+                <img src="https://img00.deviantart.net/9cd2/i/2015/217/7/0/peter_griffin__family_guy__32_by_frasier_and_niles-d94aejw.jpg" id='pete' class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
-              <div class="card">
-                <img src="https://www.liveabout.com/thmb/qL9NuJ5lmmRzMyFngIz9SyYjSiw=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Lois_f-56a00b003df78cafda9fc743.jpg" class="card-img-top" alt="..."/>
+              <div class="card center" id='lois'>
+                <img src="https://www.liveabout.com/thmb/qL9NuJ5lmmRzMyFngIz9SyYjSiw=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Lois_f-56a00b003df78cafda9fc743.jpg" id='lis' class="card-img-top" alt="..."/>
   </div>
               </div>
 
               <div class="col mb-4">
-              <div class="card">
-                <img src="https://www.liveabout.com/thmb/fc5QH6AXdNtQRwuaQevxSSiwp1k=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Stewie1_f-56a00b003df78cafda9fc746.jpg" class="card-img-top" alt="..."/>
+              <div class="card right" id='stewie2'>
+                <img src="https://www.liveabout.com/thmb/fc5QH6AXdNtQRwuaQevxSSiwp1k=/520x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fGuy2006_Stewie1_f-56a00b003df78cafda9fc746.jpg" id='stew2' class="card-img-top" alt="..."/>
   </div>
               </div>
 
