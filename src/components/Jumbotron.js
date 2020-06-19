22333e964128f1f01394bacc69275234b800f921
Adam SOudani <asoudani@email.com>
2020-06-18 17:25:31 -0700

    fixed jumbotron issues

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 export default function Jumbotron() {
+
+    const divStyle = {
+        backgroundImage: "url('https://cdn.pixabay.com/photo/2015/12/01/15/43/black-1072366_1280.jpg ')",
+        color: 'white'
+      };
+
     return (
         <div>
-            <div className="jumbotron .jumbotron-fluid .container-fluid ">
+            <div style={divStyle} className="jumbotron .jumbotron-fluid .container-fluid ">
         <h1 className="display-4">Clicky Game!</h1>
         <p className="lead"><i>Family Guy Edition</i></p>
         <hr className="my-4"></hr>
