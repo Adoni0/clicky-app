f1d6576bc3b83a3e20493750dda12cfdbe59729e
Adam SOudani <asoudani@email.com>
2020-06-20 18:44:25 -0700

    Updates

---
+++ precache-manifest.f640a6e1bda9db997e661a1ea60f3e32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a8c290ada8d7d75cf0241f6f1c7055",
+    "url": "/clicky-app/index.html"
+  },
+  {
+    "revision": "6ee5dc1097b19afa84e4",
+    "url": "/clicky-app/static/css/main.fa03eb35.chunk.css"
+  },
+  {
+    "revision": "55ea0a1c41e3cf21f12c",
+    "url": "/clicky-app/static/js/2.378b37ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-app/static/js/2.378b37ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee5dc1097b19afa84e4",
+    "url": "/clicky-app/static/js/main.06bc0e99.chunk.js"
+  },
+  {
+    "revision": "766316036b371429b96c",
+    "url": "/clicky-app/static/js/runtime-main.3de67125.js"
+  }
+]);
